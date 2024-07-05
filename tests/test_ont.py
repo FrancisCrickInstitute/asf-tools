2369b1231411055bfa1e5e2b6965539be295c61c
Chris Cheshire <chris.cheshire@crick.ac.uk>
2024-07-05 14:34:50 +0100

    disable test

--- tests/test_ont.py
+++ tests/test_ont.py
@@ -9,7 +9,6 @@ class TestOnt(unittest.TestCase):
     """Class for ont tests"""
 
     from .ont.ont_gen_demux_run import (  # type: ignore[misc]  # pylint: disable=C0415
-        test_ont_gen_demux_api_integration,
         test_ont_gen_demux_run_file_permissions,
         test_ont_gen_demux_run_folder_creation,
         test_ont_gen_demux_run_folder_creation_with_contains,
