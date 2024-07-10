1eca7c1e765f17dd22a4114614733155d3320b04
areda elezi <areda.elezi@crick.ac.uk>
2024-07-10 12:16:25 +0100

    add test of target paths list

--- tests/test_data_management.py
+++ tests/test_data_management.py
@@ -11,11 +11,11 @@ from asf_tools.io.data_management import DataManagement
 from .utils import with_temporary_folder
 
 
-class TestDataTransfer(unittest.TestCase):
+class TestDataManagement(unittest.TestCase):
     """Class for data_management tests"""
 
     @with_temporary_folder
-    def test_data_transfer_isinvalid_source(self, tmp_path):
+    def test_data_management_isinvalid_source(self, tmp_path):
         """
         Check existence of input path
         """
@@ -29,7 +29,7 @@ class TestDataTransfer(unittest.TestCase):
             dt.data_management(invalid_path, tmp_path)
 
     @with_temporary_folder
-    def test_data_transfer_isinvalid_target(self, tmp_path):
+    def test_data_management_isinvalid_target(self, tmp_path):
         """
         Check existence of target path
         """
@@ -44,7 +44,7 @@ class TestDataTransfer(unittest.TestCase):
             dt.data_management(valid_path, invalid_path)
 
     @with_temporary_folder
-    def test_data_transfer_isvalid(self, tmp_path):
+    def test_data_management_isvalid_str(self, tmp_path):
         """
         Check folder has been symlinked correctly
         """
@@ -58,8 +58,34 @@ class TestDataTransfer(unittest.TestCase):
 
         # Assert
         run_dir_1 = os.path.join(tmp_path, "run01")
-        subprocess.run(f"ls -la {tmp_path}", shell=True, check=True)
         self.assertTrue(os.path.islink(run_dir_1))
+
+    @with_temporary_folder
+    def test_data_management_isvalid_list(self, tmp_path):
+        """
+        Check folder has been symlinked correctly
+        """
+
+        # Set up
+        dt = DataManagement()
+        data_path = "tests/data/ont/runs/run01/"
+
+        # Create list of temporary paths
+        tmp_path1 = os.path.join(tmp_path, 'test1')
+        tmp_path2 = os.path.join(tmp_path, 'test2')
+        tmp_paths = [tmp_path1, tmp_path2]
+        # Create directories if they do not exist
+        os.makedirs(tmp_path1, exist_ok=True)
+        os.makedirs(tmp_path2, exist_ok=True)
+
+        # Test
+        dt.data_management(data_path, tmp_paths)
+
+        # Assert
+        run_dir_1 = os.path.join(tmp_path1, "run01")
+        run_dir_2 = os.path.join(tmp_path2, "run01")
+        self.assertTrue(os.path.islink(run_dir_1))
+        self.assertTrue(os.path.islink(run_dir_2))
 
         # # Set up
         # samplesheet = "./tests/data/ont/samplesheet/samplesheet.csv"
