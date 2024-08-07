ff81b92fbf652c4d864df9783945c1fc0aa86cb8
areda elezi <areda.elezi@crick.ac.uk>
2024-08-07 16:52:23 +0100

    reads dict function completed

--- asf_tools/illumina/illumina_utils.py
+++ asf_tools/illumina/illumina_utils.py
@@ -9,17 +9,19 @@ import xmltodict
 
 class IlluminaUtils:
 
-    def runinfo_parser(self, runinfo_file) -> dict:
-
-        # my $insert = {'SampleSheet_Trigger' => 'N', 'SampleSheet_TimeStamp' => $sst, 'SampleSheet' => $ss, 'Run_Date' => $date->strftime('%Y-%m-%d'), 'Date' => DateTime->now( time_zone => 'Europe/London' ), 'End_Type' => $end_type}
-
+    def runinfo_xml_to_dict(self, runinfo_file) -> dict:
         with open(runinfo_file, 'r', encoding="utf-8") as runinfo_file:
             runinfo_file_content = runinfo_file.read()
+            # return error if file==empty
 
         full_runinfo_dict = xmltodict.parse(runinfo_file_content)
+        return full_runinfo_dict
 
-        run_id = full_runinfo_dict["RunInfo"]["Run"]["@Id"]
-        instrument = full_runinfo_dict["RunInfo"]["Run"]["Instrument"]
+    def filter_runinfo(self, runinfo_dict: dict) -> dict:
+
+        # Extract info from the dictionary as required
+        run_id = runinfo_dict["RunInfo"]["Run"]["@Id"]
+        instrument = runinfo_dict["RunInfo"]["Run"]["Instrument"]
 
         machine_mapping = {
             '^M': 'MiSeq',
@@ -29,7 +31,6 @@ class IlluminaUtils:
             '^A': 'NovaSeq',
             '^LH': 'NovaSeqX'
         }
-
         for pattern, machine_name in machine_mapping.items():
             if re.match(pattern, instrument):
                 machine = machine_name
@@ -40,3 +41,46 @@ class IlluminaUtils:
         print(runinfo_dict)
         return runinfo_dict
 
+    def filter_readinfo(self, runinfo_dict: dict) -> dict:
+
+        run_id = runinfo_dict["RunInfo"]["Run"]["@Id"] # returns a string
+        reads_fullinfo = runinfo_dict["RunInfo"]["Run"]["Reads"] # returns a dict
+
+        # Extract single or paired end info for each read
+        end_type_count = 0
+        read_data = []
+        for read in reads_fullinfo['Read']:
+            number = read['@Number']
+            num_cycles = read['@NumCycles']
+            is_indexed_read = read['@IsIndexedRead']
+
+            if is_indexed_read == 'N':
+                # read_data[f'Read {number}'] = f'{num_cycles} Seq'
+                read_data.append({"read": f'Read {number}', "num_cycles": f'{num_cycles} Seq'})
+                end_type_count += 1
+            elif is_indexed_read == 'Y':
+                # read_data[f'Read {number}'] = f'{num_cycles} Ind'
+                read_data.append({"read": f'Read {number}', "num_cycles": f'{num_cycles} Seq'})
+
+        end_type = "SR"
+        if end_type_count > 1:
+            end_type = "PE"
+
+        # Collect the extracted info in a single dictionary
+        readinfo_dict = {}
+        readinfo_dict["run_id"] = run_id
+        readinfo_dict["end_type"] = end_type
+        readinfo_dict["reads"] = read_data
+        print(readinfo_dict)
+
+        return readinfo_dict
+    
+    def merge_runinfo_dict_fromfile(self, runinfo_file) -> dict:
+        original_dict = self.runinfo_xml_to_dict(runinfo_file)
+        filtered_dict = self.filter_runinfo(original_dict)
+        reads_dict = self.filter_readinfo(original_dict)
+
+        merged_dict = filtered_dict
+
+
+# my $insert = {'SampleSheet_Trigger' => 'N', 'SampleSheet_TimeStamp' => $sst, 'SampleSheet' => $ss, 'End_Type' => $end_type}
--- tests/test_illumina.py
+++ tests/test_illumina.py
@@ -8,15 +8,54 @@ from asf_tools.illumina.illumina_utils import IlluminaUtils
 
 class TestRunInfoParse(unittest.TestCase):
     """Class for parse_runinfo tests"""
-    
-    def test_runinfo_parser(self):
+# runinfo_xml_to_dict - need to test for: none, empty file, something other than file as an input
+# filter_runinfo - need to test for: the presence of runid,instrument info, machine that isn't in the mapping dict
+# filter_readinfo - need to test for: the presence of runid and reads
+
+    def test_runinfo_xml_to_dict_isnone(self):
         # Set up
         iu = IlluminaUtils()
         file = "./tests/data/illumina/RunInfo.xml"
 
         # Test
-        xml_info = iu.runinfo_parser(file)
+        xml_info = iu.runinfo_xml_to_dict(file)
         # print(xml_info)
 
         # Assert
         assert xml_info == []
+
+    def test_runinfo_xml_to_dict_isvalid(self):
+        # Set up
+        iu = IlluminaUtils()
+        file = "./tests/data/illumina/RunInfo.xml"
+
+        # Test
+        xml_info = iu.runinfo_xml_to_dict(file)
+        # print(xml_info)
+
+        # Assert
+        assert xml_info == []
+
+    # def test_runinfo_parser(self):
+    #     # Set up
+    #     iu = IlluminaUtils()
+    #     file = "./tests/data/illumina/RunInfo.xml"
+
+    #     # Test
+    #     xml_info = iu.runinfo_parser(file)
+    #     # print(xml_info)
+
+    #     # Assert
+    #     assert xml_info == []
+    def test_filter_readinfo(self):
+        # Set up
+        iu = IlluminaUtils()
+        file = "./tests/data/illumina/RunInfo.xml"
+
+        # Test
+        xml_info = iu.runinfo_xml_to_dict(file)
+        read_info = iu.filter_readinfo(xml_info)
+        # print(xml_info)
+
+        # Assert
+        assert read_info == []
