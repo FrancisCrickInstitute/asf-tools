18278d2c7dfef823e2e9d0c4c83c3ed52d4ce354
areda elezi <areda.elezi@crick.ac.uk>
2024-08-08 21:41:03 +0100

    added flexibility to filter functions

--- asf_tools/illumina/illumina_utils.py
+++ asf_tools/illumina/illumina_utils.py
@@ -12,17 +12,100 @@ import xmltodict
 class IlluminaUtils:
 
     def runinfo_xml_to_dict(self, runinfo_file) -> dict:
+        """
+        Converts an XML RunInfo file into a Python dictionary.
+
+        This method reads the contents of a RunInfo XML file, parses it using the
+        `xmltodict` library, and returns the parsed data as a dictionary.
+
+        Args:
+            runinfo_file (str): The file path to the RunInfo XML file.
+
+        Returns:
+            dict: A dictionary representation of the XML file content.
+        """
         with open(runinfo_file, "r", encoding="utf-8") as runinfo_file:
             runinfo_file_content = runinfo_file.read()
 
         full_runinfo_dict = xmltodict.parse(runinfo_file_content)
         return full_runinfo_dict
 
+    def find_key_recursively(self, d: dict, target_key: str) -> list:
+        """
+        Recursively searches for a target key in a nested dictionary.
+        
+        Args:
+        d (dict): The dictionary to search through.
+        target_key (str): The key to find.
+
+        Returns:
+        list: A list of values associated with the target key.
+        """
+        results = []
+
+        # If the dictionary has the target key at the top level, add the value to results
+        if target_key in d:
+            results.append(d[target_key])
+
+        # Otherwise, continue searching recursively in nested dictionaries or lists
+        for key, value in d.items():
+            if isinstance(value, dict):
+                results.extend(self.find_key_recursively(value, target_key))
+            elif isinstance(value, list):
+                for item in value:
+                    if isinstance(item, dict):
+                        results.extend(self.find_key_recursively(item, target_key))
+
+        return results
+
+    def extract_matching_item_from_xmldict(self, item_list: list, item_name: str):
+        """
+        Extracts the first occurrence of a specified item from a list.
+
+        This method searches for the first occurrence of a given item within a list
+        by recursively searching the list structure. If the item is found, it returns
+        the first instance. If the item is not found, it raises a `ValueError`.
+
+        Args:
+            item_list (list): The list to search within.
+            item_name (str): The name of the item to search for.
+
+        Returns:
+            str: The first occurrence of the specified item in the list.
+
+        Raises:
+            ValueError: If the item is not found in the list.
+        """
+        item_results = self.find_key_recursively(item_list, item_name)
+        item = item_results[0] if item_results else None
+
+        if item is None:
+            raise ValueError(f"No {item_name} found in the XML structure.")
+
+        return item
+
     def filter_runinfo(self, runinfo_dict: dict) -> dict:
+        """
+        Filters and restructures information from a RunInfo dictionary.
+
+        This method extracts specific information from the provided RunInfo dictionary, 
+        such as the run ID and instrument name. It then maps the instrument to its 
+        corresponding machine type using predefined patterns. The resulting dictionary 
+        includes the current date and time, the run ID, the instrument name, and the 
+        machine type.
+
+        Args:
+            runinfo_dict (dict): The dictionary containing the RunInfo data.
+
+        Returns:
+            dict: A dictionary containing filtered and structured RunInfo data, 
+                including the current date, run ID, instrument, and machine type.
+        """
 
         # Extract info from the dictionary as required
-        run_id = runinfo_dict["RunInfo"]["Run"]["@Id"]
-        instrument = runinfo_dict["RunInfo"]["Run"]["Instrument"]
+        run_id = self.extract_matching_item_from_xmldict(runinfo_dict, "@Id")
+        instrument = self.extract_matching_item_from_xmldict(runinfo_dict, "Instrument")
+
 
         machine_mapping = {"^M": "MiSeq", "^K": "HiSeq 4000", "^D": "HiSeq 2500", "^N": "NextSeq", "^A": "NovaSeq", "^LH": "NovaSeqX"}
         for pattern, machine_name in machine_mapping.items():
@@ -37,23 +120,22 @@ class IlluminaUtils:
 
     def filter_readinfo(self, runinfo_dict: dict) -> dict:
 
-        run_id = runinfo_dict["RunInfo"]["Run"]["@Id"]  # returns a string
-        reads_fullinfo = runinfo_dict["RunInfo"]["Run"]["Reads"]  # returns a dict
+        run_id = self.extract_matching_item_from_xmldict(runinfo_dict, "@Id")
+        reads_fullinfo = self.extract_matching_item_from_xmldict(runinfo_dict, "Reads")
 
         # Extract single or paired end info for each read
         end_type_count = 0
         read_data = []
+
         for read in reads_fullinfo["Read"]:
             number = read["@Number"]
             num_cycles = read["@NumCycles"]
             is_indexed_read = read["@IsIndexedRead"]
 
             if is_indexed_read == "N":
-                # read_data[f'Read {number}'] = f'{num_cycles} Seq'
                 read_data.append({"read": f"Read {number}", "num_cycles": f"{num_cycles} Seq"})
                 end_type_count += 1
             elif is_indexed_read == "Y":
-                # read_data[f'Read {number}'] = f'{num_cycles} Ind'
                 read_data.append({"read": f"Read {number}", "num_cycles": f"{num_cycles} Seq"})
 
         end_type = "SR"
@@ -65,9 +147,61 @@ class IlluminaUtils:
         readinfo_dict["run_id"] = run_id
         readinfo_dict["end_type"] = end_type
         readinfo_dict["reads"] = read_data
-        # print(readinfo_dict)
+        print(readinfo_dict)
 
         return readinfo_dict
+
+
+    # def filter_runinfo(self, runinfo_dict: dict) -> dict:
+
+    #     # Extract info from the dictionary as required
+    #     run_id = runinfo_dict["RunInfo"]["Run"]["@Id"]
+    #     instrument = runinfo_dict["RunInfo"]["Run"]["Instrument"]
+
+    #     machine_mapping = {"^M": "MiSeq", "^K": "HiSeq 4000", "^D": "HiSeq 2500", "^N": "NextSeq", "^A": "NovaSeq", "^LH": "NovaSeqX"}
+    #     for pattern, machine_name in machine_mapping.items():
+    #         if re.match(pattern, instrument):
+    #             machine = machine_name
+
+    #     current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    #     runinfo_dict = {"current_date": current_datetime, "run_id": run_id, "instrument": instrument, "machine": machine}
+    #     # print(runinfo_dict)
+    #     return runinfo_dict
+
+    # def filter_readinfo(self, runinfo_dict: dict) -> dict:
+
+    #     run_id = runinfo_dict["RunInfo"]["Run"]["@Id"]  # returns a string
+    #     reads_fullinfo = runinfo_dict["RunInfo"]["Run"]["Reads"]  # returns a dict
+
+    #     # Extract single or paired end info for each read
+    #     end_type_count = 0
+    #     read_data = []
+    #     for read in reads_fullinfo["Read"]:
+    #         number = read["@Number"]
+    #         num_cycles = read["@NumCycles"]
+    #         is_indexed_read = read["@IsIndexedRead"]
+
+    #         if is_indexed_read == "N":
+    #             # read_data[f'Read {number}'] = f'{num_cycles} Seq'
+    #             read_data.append({"read": f"Read {number}", "num_cycles": f"{num_cycles} Seq"})
+    #             end_type_count += 1
+    #         elif is_indexed_read == "Y":
+    #             # read_data[f'Read {number}'] = f'{num_cycles} Ind'
+    #             read_data.append({"read": f"Read {number}", "num_cycles": f"{num_cycles} Seq"})
+
+    #     end_type = "SR"
+    #     if end_type_count > 1:
+    #         end_type = "PE"
+
+    #     # Collect the extracted info in a single dictionary
+    #     readinfo_dict = {}
+    #     readinfo_dict["run_id"] = run_id
+    #     readinfo_dict["end_type"] = end_type
+    #     readinfo_dict["reads"] = read_data
+    #     # print(readinfo_dict)
+
+    #     return readinfo_dict
 
     def merge_runinfo_dict_fromfile(self, runinfo_file) -> dict:
         original_dict = self.runinfo_xml_to_dict(runinfo_file)
--- tests/test_illumina.py
+++ tests/test_illumina.py
@@ -12,7 +12,6 @@ from asf_tools.illumina.illumina_utils import IlluminaUtils
 
 class TestRunInfoParse(unittest.TestCase):
     """Class for parse_runinfo tests"""
-
     # filter_runinfo - need to test for: the presence of runid,instrument info, machine that isn't in the mapping dict
     # filter_readinfo - need to test for: the presence of runid and reads
 
@@ -67,30 +66,18 @@ class TestRunInfoParse(unittest.TestCase):
         with self.assertRaises(ExpatError):
             iu.runinfo_xml_to_dict(invalid_file)
 
-    # def test_runinfo_parser(self):
-    #     # Set up
-    #     iu = IlluminaUtils()
-    #     file = "./tests/data/illumina/RunInfo.xml"
+    def test_filter_runinfo_isnone(self):
+        """
+        Pass None to method
+        """
 
-    #     # Test
-    #     xml_info = iu.runinfo_parser(file)
-    #     # print(xml_info)
-
-    #     # Assert
-    #     assert xml_info == []
-
-    def test_filter_readinfo(self):
         # Set up
         iu = IlluminaUtils()
-        file = "./tests/data/illumina/RunInfo.xml"
 
-        # Test
-        xml_info = iu.runinfo_xml_to_dict(file)
-        read_info = iu.filter_readinfo(xml_info)
-        # print(xml_info)
+        # Test and Assert
+        with self.assertRaises(TypeError):
+            iu.filter_runinfo(None)
 
-        # Assert
-        assert read_info == []
 
     def test_merge_runinfo_dict_fromfile(self):
         # Set up
@@ -181,3 +168,84 @@ class TestRunInfoParseWithFixtures:
 
         # Assert
         assert xml_info == expected_dict
+
+    @pytest.mark.parametrize("file,expected_dict", [("./tests/data/illumina/RunInfo.xml",{})])
+    def test_filter_readinfo_isvalid(self, file, expected_dict):
+        """
+        Pass a valid XML file and test expected values in the dictionary output
+        """
+
+        # Set up
+        iu = IlluminaUtils()
+
+        # Test
+        xml_info = iu.runinfo_xml_to_dict(file)
+        read_info = iu.filter_readinfo(xml_info)
+        # print(xml_info)
+
+        # Assert
+        assert read_info == expected_dict
+
+
+    @pytest.mark.parametrize("input_dict,expected_dict",
+        [({
+            "@Version": "6",
+            "Run": {
+                "@Id": "20240711_LH00442_0033_A22MKK5LT3",
+                "@Number": "33",
+                "Flowcell": "22MKK5LT3",
+                "Instrument": "LH00442",
+                "Date": "2024-07-11T18:44:29Z",
+                "Reads": {
+                    "Read": [
+                        {"@Number": "1", "@NumCycles": "151", "@IsIndexedRead": "N", "@IsReverseComplement": "N"},
+                        {"@Number": "2", "@NumCycles": "10", "@IsIndexedRead": "Y", "@IsReverseComplement": "N"},
+                        {"@Number": "3", "@NumCycles": "10", "@IsIndexedRead": "Y", "@IsReverseComplement": "Y"},
+                        {"@Number": "4", "@NumCycles": "151", "@IsIndexedRead": "N", "@IsReverseComplement": "N"},
+                    ]
+                },
+            "FlowcellLayout": {
+                "@LaneCount": "8",
+                "@SurfaceCount": "2",
+                "@SwathCount": "2",
+                "@TileCount": "98",
+                "TileSet": {
+                    "@TileNamingConvention": "FourDigit",
+                    "Tiles": {
+                        "Tile": [
+                            "1_1101",
+                            "2_1101",
+                            "3_1101",
+                            "4_1101",
+                            "5_1101",
+                            "6_1232",
+                            "7_1101",
+                            "8_1101",
+                            "1_2298",
+                            "2_2101",
+                            "3_2101",
+                            "4_2101",
+                            "5_2101",
+                            "6_2101",
+                            "7_2101",
+                            "8_2101",]
+                            },
+                        },
+                    },
+            "ImageDimensions": {"@Width": "5120", "@Height": "2879"},
+            "ImageChannels": {"Name": ["blue", "green"]},
+            },
+        }, "empty") ],
+    )
+    def test_filter_runinfo_isinvalid(self, input_dict, expected_dict):
+        # Set up
+        iu = IlluminaUtils()
+
+        # Test
+        run_info = iu.filter_runinfo(input_dict)
+        # print(xml_info)
+
+        # Assert
+        assert run_info == expected_dict
+        # with self.assertRaises(ExpatError):
+        #     iu.filter_runinfo(xml_info)
