eeee5f3b4c822f880fb2c4a42402237d32daabe2
areda elezi <areda.elezi@crick.ac.uk>
2024-08-16 16:10:34 +0100

    archive file matches nemo

--- asf_tools/io/data_management.py
+++ asf_tools/io/data_management.py
@@ -262,7 +262,7 @@ class DataManagement:
                     formatted_mtime = dir_mtime.strftime("%B %d, %Y, %H:%M:%S UTC")
                     days_since_modified = (current_time - dir_mtime).days
 
-                    if not check_file_exist(dir_path, "archived_data"):
+                    if not check_file_exist(dir_path, "archive_readme"):
                         old_folders[dir_name] = {"path": dir_name, "days_since_modified": days_since_modified, "last_modified": formatted_mtime}
 
         return old_folders
