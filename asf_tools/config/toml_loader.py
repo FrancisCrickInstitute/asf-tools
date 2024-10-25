f7a4c537543d6fe2b7fdbfd732efa6e55681bc18
Chris Cheshire <chris.cheshire@crick.ac.uk>
2024-10-25 12:12:43 +0100

    Add toml loader

---
+++ asf_tools/config/toml_loader.py
@@ -0,0 +1,34 @@
+"""
+Load from toml file either in a specified file path or from the default home directory.
+"""
+
+import os
+import toml
+
+
+def load_toml_file(file_path: str = None, file_name = "crick.toml") -> dict:
+    """
+    Load a TOML file from a specified file path or default to a file name in the user's home directory.
+
+    Args:
+        file_path (str, optional): The full path to the TOML file. Defaults to None.
+        file_name (str, optional): The name of the TOML file to load from the user's home directory if 
+        file_path is not provided. Defaults to "crick.toml".
+
+    Returns:
+        dict: The contents of the TOML file as a dictionary.
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+        toml.TomlDecodeError: If the file is not a valid TOML file.
+    """
+
+    # construct the file path
+    resolved_path = file_path
+    if not resolved_path:
+        home_dir = os.path.expanduser("~")
+        resolved_path = os.path.join(home_dir, file_name)
+
+    # load the toml file
+    with open(file_path, "r", encoding="UTF-8") as file:
+        return toml.load(file)
---
+++ tests/test_config_toml_loader.py
@@ -0,0 +1,39 @@
+"""
+Tests for the toml_loader module.
+"""
+
+# pylint: disable=missing-function-docstring,missing-class-docstring
+
+import os
+import toml
+import unittest
+from unittest.mock import patch, mock_open
+from asf_tools.config.toml_loader import load_toml_file
+
+class TestTomlLoader(unittest.TestCase):
+
+    def test_load_toml_file_with_file_path(self):
+        mock_data = {"key": "value"}
+        with patch("builtins.open", mock_open(read_data=toml.dumps(mock_data))):
+            result = load_toml_file(file_path="/path/to/file.toml")
+            self.assertEqual(result, mock_data)
+
+    def test_load_toml_file_with_file_name(self):
+        mock_data = {"key": "value"}
+        home_dir = os.path.expanduser("~")
+        file_path = os.path.join(home_dir, "crick.toml")
+        with patch("builtins.open", mock_open(read_data=toml.dumps(mock_data))):
+            with patch("os.path.expanduser", return_value=home_dir):
+                result = load_toml_file()
+                self.assertEqual(result, mock_data)
+
+    def test_load_toml_file_with_invalid_toml(self):
+        invalid_toml_data = "invalid_toml"
+        with patch("builtins.open", mock_open(read_data=invalid_toml_data)):
+            with self.assertRaises(toml.TomlDecodeError):
+                load_toml_file(file_path="/path/to/invalid.toml")
+
+    def test_load_toml_file_file_not_found(self):
+        with patch("builtins.open", side_effect=FileNotFoundError):
+            with self.assertRaises(FileNotFoundError):
+                load_toml_file(file_path="/path/to/nonexistent.toml")
