284ef11392bfe441b72fdd1162b403b7f0d67dac
Chris Cheshire <chris.cheshire@crick.ac.uk>
2025-03-10 10:55:43 +0000

    Fix tests

--- asf_tools/io/data_management.py
+++ asf_tools/io/data_management.py
@@ -4,7 +4,7 @@ Helper functions for data management
 
 import logging
 import os
-import re
+# import re
 import subprocess
 from datetime import datetime, timedelta, timezone
 from enum import Enum
